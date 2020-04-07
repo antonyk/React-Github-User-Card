cf503d9aaac3711cbed70e6996f8ad2d4f959b2e
Anthony K <antonyk@users.noreply.github.com>
2020-04-07 18:45:52 -0400

    added axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div><h3>GitHub Network</h3></div>
+          <div className='header-spacer'></div>
+        </header>
+        <section className='app-body'>
+          <h1>GitHub Cards Demo</h1>
+          <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer">
+            <span>Hello World</span>
+          </a>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
