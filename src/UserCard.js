9d7bbea886b7820b91dd3561dd69181de91b97f6
Anthony K <antonyk@users.noreply.github.com>
2020-04-07 17:37:11 -0400

    project cleanup and card component boilerplate

---
+++ src/UserCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserCard extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  render() {
+
+  }
+}
+export default UserCard;
