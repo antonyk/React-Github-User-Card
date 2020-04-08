dda77a6d3e8189cd3c3bcb17f8a555cf2a775e6e
Anthony K <antonyk@users.noreply.github.com>
2020-04-07 22:56:43 -0400

    MVP comleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import UserCard from './UserCard'
+
+const defaultUsername = 'antonyk';
+const pageSize = 5;
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      username: defaultUsername,
+      userData: {},
+      followersData: [],
+      followingData: [],
+    }
+  }
+
+  componentDidMount(prevProps, prevState) {
+    this.getGithubData();
+
+  }
+
+  getGithubData() {
+    axios.get(`https://api.github.com/users/${this.state.username}`)
+    .then(result => {
+      // console.log(result)
+      this.setState({
+        userData: result.data
+      });
+      this.getUserFollowersData(this.state.username)
+      this.getUserFollowingData(this.state.username)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  getUserFollowersData(username) {
+    // get followers
+    axios.get(`https://api.github.com/users/${username}/followers`)
+    .then(result => {
+      this.setState({
+        followersData: result.data
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  getUserFollowingData(username) {
+    // get following
+    axios.get(`https://api.github.com/users/${username}/following`)
+    .then(result => {
+      this.setState({
+        followingData: result.data
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
   render() {
@@ -17,10 +73,14 @@ class App extends Component {
           <div className='header-spacer'></div>
         </header>
         <section className='app-body'>
-          <h1>GitHub Cards Demo</h1>
-          <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer">
-            <span>Hello World</span>
-          </a>
+          <h1>GitHub Cards</h1>
+          <h2>Main User</h2>
+          <UserCard key={this.state.userData.login} user={this.state.userData} />
+          
+          <h2>Followers</h2>
+          {this.state.followersData.map(item => <UserCard key={item.login} user={item}/> )}
+          <h2>Following</h2>
+          {this.state.followingData.map(item => <UserCard key={item.login} user={item}/> )}
         </section>
       </div>
     );
--- src/UserCard.js
+++ src/UserCard.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 
 class UserCard extends Component {
-  constructor(props) {
-    super();
-    this.state = {};
-  }
 
   render() {
-
+    return (
+      <div>
+        <div className='card-title'>
+          <h4>{this.props.user.login}</h4>
+        </div>
+        <div className='profile-img'>
+          <img src={this.props.user.avatar_url} alt={`profile for user ${this.props.user.login}`} />
+        </div>
+        <div>
+          <a className="App-link" href={this.props.user.repos_url} rel="noopener noreferrer">
+            <span>GitHub Repos</span>
+          </a>
+        </div>
+      </div>
+    )
   }
 }
 export default UserCard;
